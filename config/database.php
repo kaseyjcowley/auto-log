00910465882606b6364ddcd9230970f1abbd128c
Kasey Cowley <kchunterdeluxe@gmail.com>
2016-10-03 21:44:53 -0600

    Setup testing database

--- config/database.php
+++ config/database.php
@@ -52,13 +52,21 @@ return [
             'prefix' => '',
         ],
 
-        'sqlite_testing' => [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
+            'strict' => true,
+            'engine' => null,
         ],
 
-        'mysql' => [
+        'mysql_testing' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
